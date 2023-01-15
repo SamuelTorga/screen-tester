dc2146dbafa1c0f380a1044ccd44c4ac1009d689
Samuel Torga <samueltorga@gmail.com>
2023-01-15 14:09:27 -0300

    Navigation component
    Creating navigation component

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,39 +1,9 @@
 import React from 'react'
 
+import Navigation from '../layout/Navigation'
+
 const Root: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href={'/angle'}>Angle</a>
-        </li>
-        <li>
-          <a href={'/color-distance'}>Color Distance</a>
-        </li>
-        <li>
-          <a href={'/defects'}>Defects</a>
-        </li>
-        <li>
-          <a href={'/gamma'}>Gamma</a>
-        </li>
-        <li>
-          <a href={'/gradients'}>Gradients</a>
-        </li>
-        <li>
-          <a href={'/pattern'}>Pattern</a>
-        </li>
-        <li>
-          <a href={'/response-time'}>Response Time</a>
-        </li>
-        <li>
-          <a href={'/sharpness'}>Sharpness</a>
-        </li>
-        <li>
-          <a href={'/uniformity'}>Uniformity</a>
-        </li>
-      </ul>
-    </nav>
-  )
+  return <Navigation />
 }
 
 export default Root
